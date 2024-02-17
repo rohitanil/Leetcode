dc099f783585dd2ad72cd5d038e622f85ea825fc
Rohit Anil <rohitanil7121993@gmail.com>
2024-02-16 20:13:17 -0500

    check if subsequence with sum k exists

--- src/subsequences.py
+++ src/subsequences.py
@@ -2,9 +2,11 @@
 Program to
     1. print all subsequences of a list
     2. print all subsequences equal to sum k
+    3. check if subsequence with sum k exists
 """
 
 
+# 1
 def subsequences(idx, lst, arr):
     if idx >= len(arr):
         result.append(lst[:])
@@ -17,6 +19,7 @@ def subsequences(idx, lst, arr):
     subsequences(idx + 1, lst[:], arr)
 
 
+# 2
 def subsequenceSum(idx, lst, curr_sum, target, arr):
     if idx >= len(arr):
         if curr_sum == target:
@@ -24,20 +27,39 @@ def subsequenceSum(idx, lst, curr_sum, target, arr):
         return
     lst.append(arr[idx])
     curr_sum += arr[idx]
-    subsequenceSum(idx+1, lst, curr_sum, target, arr)
+    subsequenceSum(idx + 1, lst, curr_sum, target, arr)
     lst.remove(arr[idx])
     curr_sum -= arr[idx]
     subsequenceSum(idx + 1, lst[:], curr_sum, target, arr)
 
 
+# 3
+def checkAnySubsequenceWithSumK(idx, curr_sum, target, arr):
+    if idx >= len(arr):
+        if curr_sum == target:
+            return True
+        else:
+            return False
+
+    curr_sum += arr[idx]
+    if checkAnySubsequenceWithSumK(idx + 1, curr_sum, target, arr):
+        return True
+
+    curr_sum -= arr[idx]
+    if checkAnySubsequenceWithSumK(idx + 1, curr_sum, target, arr):
+        return True
+    return False
+
 
 if __name__ == '__main__':
     result = []
     result2 = []
+
     inp = [1, 2, 1]
-    target = 2
+    tar = 2
 
     subsequences(0, [], inp)
     print("Subsequences: ", result)
-    subsequenceSum(0, [], 0, target, inp)
-    print(f"Subsequences with sum {target}: {result2}")
+    subsequenceSum(0, [], 0, tar, inp)
+    print(f"Subsequences with sum {tar}: {result2}")
+    print(f"Subsequences with sum {tar} present {checkAnySubsequenceWithSumK(0, 2, tar, inp)}")
